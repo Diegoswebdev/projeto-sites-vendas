ed68e866e2a7b1923a1e94af47bc05354d1bea50
Diegoswebdev <diegos.pg@hotmail.com>
2024-05-04 20:09:24 -0300

    projeto concluido mas preciso aruma o js esta com um erro

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function filterPrompts() {
+    var input = document.getElementById('searchInput').value.toUpperCase();
+    var prompts = document.getElementsByClassName('prompt');
+
+    for (var i = 0; i < prompts.length; i++) {
+        var h3 = prompts[i].getElementsByTagName('h3')[0];
+        if (h3.innerHTML.toUpperCase().indexOf(input) > -1) {
+            prompts[i].style.display = "";
+        } else {
+            prompts[i].style.display = "none";
+        }
+    }
+}
